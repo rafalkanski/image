3422ab7f3ec743080a6a9b88188a80caacce917b
Rafal Kanski <rafal.kanski@schibsted.pl>
2014-09-26 07:45:58 +0200

    Revert "possibility to choose algorithm for resizing"
    This reverts commit 388e3e87a84f4ce9b3690f125f5548adefb60b8d.

--- src/Intervention/Image/Imagick/Commands/ResizeCommand.php
+++ src/Intervention/Image/Imagick/Commands/ResizeCommand.php
@@ -14,19 +14,13 @@ class ResizeCommand extends \Intervention\Image\Commands\AbstractCommand
     {
         $width = $this->argument(0)->value();
         $height = $this->argument(1)->value();
-        $algorithm = $this->argument(2)->value();
-
-        if (\is_null($algorithm)) {
-            $algorithm = \Imagick::FILTER_BOX;
-        }
-
         $constraints = $this->argument(2)->type('closure')->value();
 
         // resize box
         $resized = $image->getSize()->resize($width, $height,$algorithm, $constraints);
 
         // modify image
-        $image->getCore()->resizeImage($resized->getWidth(), $resized->getHeight(), $algorithm, 1);
+        $image->getCore()->resizeImage($resized->getWidth(), $resized->getHeight(), \Imagick::FILTER_TRIANGLE, 1);
 
         return true;
     }
